41e17576992398bbb6cf5f8efd83f65dd510b4d2
Craig Jenkins <mccraigj@gmail.com>
2018-06-12 15:17:47 +0100

    Implementing a Note Application

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Notes from './components/Notes';
+
+import uuid from 'uuid';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+
+    const { notes } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+}
+
+export default App;
